df14d35667aae2d2269828cf9e74bf491ca87fac
codeFabFit <fabgar14@gmail.com>
2023-12-03 00:51:31 -0500

    making snake and body when eatting food

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const playBoard = document.querySelector(".play-board");
 
-let foodX= 13, foodY = 10;
+let foodX, foodY;
+let snakeX = 5, snakeY = 10;
+let snakeBody = []
+let velocityX = 0, velocityY = 0;
 
 const changeFoodPosition = () => {
     // this passing a random 0 thru 30 as food positiion
@@ -9,9 +12,43 @@ const changeFoodPosition = () => {
 
 }
 
-const initGame = () => {
-    let htmlMarkup =`<div class="food" style="grid-area:${foodY}/${foodX}"></div>`
-    playBoard.innerHTML = htmlMarkup;
+const changeDirection = (e) => {
+    if(e.key === "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
+    
 }
 
-initGame();
+const initGame = () => {
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`
+    
+    // making sure if the snake hit the food 
+    if(snakeX === foodX && snakeY === foodY) {
+        changeFoodPosition();
+        snakeBody.push([foodX, foodY]); // pushing the food position to body array 
+        console.log(snakeBody);
+    }
+    // updates snake head posiiton based on the current velocity
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    for (let i= 0; i < snakeBody.length; i++){
+         htmlMarkup += `<div class="head" style="grid-area:${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`
+    
+    }
+    playBoard.innerHTML = htmlMarkup;
+}
+   
+changeFoodPosition();
+setInterval(initGame, 125);
+document.addEventListener("keydown", changeDirection);
